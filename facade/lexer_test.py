57ac14f1aeaba920c2af304e1262e9a8ca0a89cf
Erich Blume <blume.erich@gmail.com>
2012-01-04 20:09:21 -0800

    Added more robust lexer testing, work on ABCs for parser

---
+++ facade/lexer_test.py
@@ -0,0 +1,57 @@
+# lexer_test.py - unit tests for lexer.py
+
+"""This module provides unit tests for the ``facade.lexer``
+module.
+
+As with all unit test modules, the tests it contains can be executed in many
+ways, but most easily by going to the project root dir and executing
+``python3 setup.py nosetests``.
+"""
+
+import unittest
+
+import facade.lexer as fl
+
+class LexerTester(unittest.TestCase):
+    """Test harness for ``facade.lexer.Lexer`` class.
+
+    """
+
+    def setUp(self):
+        """Create the testing environment"""
+        pass
+
+    def tearDown(self):
+        """Remove the testing environment"""
+        pass
+
+    def test_SimpleTokens(self):
+        """lexer.py: Test a simple lexicon"""
+        l = fl.Lexer()
+        l.addtoken('WORD',r'[a-zA-Z]+')
+        l.addtoken('NUMBER',r'[0-9]+')
+
+        input = """
+        this is
+        a
+        tes42t
+        """
+
+        tokens = [t for t in l.lex(input)]
+        
+        self.assertEqual(len(tokens),6)
+        self.assertEqual(tokens[4].match,"42")
+
+    def test_greedy(self):
+        """lexer.py: Test greedy matching"""
+        l = fl.Lexer()
+        l.addtoken('ONE',r'\*')
+        l.addtoken('TWO',r'\*\*')
+
+        tokens = [t for t in l.lex(" * ")]
+        self.assertEqual(len(tokens),1)
+        self.assertEqual(tokens[0].name,"ONE")
+
+        tokens = [t for t in l.lex(" ** ")]
+        self.assertEqual(len(tokens),1)
+        self.assertEqual(tokens[0].name,"TWO")
---
+++ facade/parser.py
@@ -0,0 +1,145 @@
+""" parser.py - CFG parsing with python functions as semantic actions
+
+Create Parser objects, which take tokenized input (via lexer.py) and use
+different parsing methods to synthesize BNF rules in to productions with
+semantic python actions. All semantic actions are python functions that take a
+list as an argument (representing the values of sub-expressions) and return a
+value as the result for that expression.
+
+Use the helper method ``parser()`` to create a Parser object if you don't
+care about what algorithm it will use. Otherwise, you cannot instantiate a
+Parser object directly, but must instead use on of its subclasses, each of
+which uses a slightly different algorithm. (It is the author's plan to use this
+setup as a way to read through the 'Purple Dragon Book' - Aho, Lam, Sethi, and
+Ullman.
+"""
+
+
+class Parser:
+    def __init__(self, lexer):
+        """LALR parser of arbitrary BNF-like rules.
+    
+        Use ``addrule`` to add rules with semantic actions. Use ``parse`` to
+        execute parsing. Parsing is immediate and returns no value, unlike other
+        LALR parsers which might work stepwise - this is because this class takes
+        advantage of python generator functions.
+    
+        The following example emulates this simple grammar (in bison/YACC form:)
+    
+        expr : expr '+' term   { $$ = $1 + $3; }
+             | expr '-' term   { $$ = $1 - $3; }
+             | term            { $$ = $1; }
+             ;
+        
+        term : '(' expr ')'    { $$ = $2; }
+             | num             { $$ = $1; }
+             ;
+    
+        num : '0'              {$$ = 0; }
+            | '1'              {$$ = 1; }
+            [.... etc, for each number 0-0 ....]
+            ;
+    
+        >>> l = Lexer()
+        >>> l.addtoken('NUMBER',r'[0-9]+')
+        >>> p = Parser(l)
+        >>>
+        >>> p.addrule('prog', "expr", lambda v: print(v[0]))
+        >>> 
+        >>> p.addrule('expr', "expr '+' term", lambda v: v[0] + v[2])
+        >>> p.addrule('expr', "expr '-' term", lambda v: v[0] - v[2])
+        >>> p.addrule('expr', "term", lambda v: v[0])
+        >>>
+        >>> p.addrule('term', " '(' expr ')' ", lambda v: v[1])
+        >>> p.addrule('term', " NUMBER ", lambda v: v[0])
+        >>>
+        >>> p.parse("5-(9+2)")
+        -6
+    
+        """
+        pass
+
+    def addrule(self,symbol,rule,action):
+        """Add a production (rule) to the grammar of the parser. Instructs the
+        parser that `symbol` can be derived using `rule`, producing a result
+        via `action`.
+
+        `symbol` is a string that must match the regular expression
+            r'[a-zA-Z]+'
+        Note that it is not allowed for a symbol to have the same name as
+        a token that might be produced by the lexer. Therefor, if you try
+        to add a rule with a `symbol` that is already the name of a token,
+        ValueError will be raised immediatly, and the rule will not be added.
+
+        `rule` is a string that has whitespace seperated terminal and
+        nonterminal symbols (see below). It may also be a list of such strings
+        (with all whitespace stripped). In other words, if the rule is
+            "foo '*' bar"
+        you could equivalently use rule.split() - both work just as well.
+
+        `action` is a function (often a lambda expression, but there is no
+        such requirement) that takes a list as input and may return some value.
+        The list will be filled with returned values of the derivation actions
+        of the symbols in the rule, in order, starting from 0. (See the example
+        in the Parser class documentation for a more clear explanation.)
+        Alternately, action may be any other value, in which case that value
+        will be used for upper derivation actions direcly.
+
+        You may define rules that derive the same symbol any number of times,
+        but keep in mind that what you are doing is defining multiple
+        derivations. All symbols used in a rule must have a derivation before
+        parsing, although you can use a symbol in a new rule before it has
+        a derivation (so long as you eventually give it a derivation).
+
+        Terminal and Nonterminal Symbols
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        Recall that the `rule` parameter is a whitespace-seperated strings of
+        'symbols' (or a list of symbols). A symbol is defined as either:
+
+        1. All of the named tokens in the lexer, which have names conforming to
+          the regex found in lexer._token_ident (at this writing,
+          r'[a-zA-Z][_a-zA-Z0-9]*').
+        2. String literals, which are identified in the rule as any single
+          character between two single quotes (including, possibly, a single
+          quote itself - which would be three single quotes one after another.)
+        3. Any nonterminal symbol (see below), although keep in mind that all
+          nonterminal symbols must have at least one derivation before parsing
+          can commense.
+
+        1 and 2 describe "terminal symbols", in that they have no derivations
+        and exist directly in the input stream. 3, the nontermimal symbols,
+        are exactly the set of strings called "symbol" that get passed to this
+        function (`addrule`).
+
+        Note that string literals are only supported when using
+        ``lexer.Lexer``'s `report_literals` option (which is on by default).
+        It is also perfectly acceptable to use "LITERAL" as a named token, but
+        recall that this will match *any* string literal, not just the specific
+        one you could have given if you had used the single-quote shortcut.
+
+        It is currently very combursome to try and match multi-character string
+        literals, so it is generally suggested to wrap such keywords in a
+        token specificically for that keyword instead.
+
+        """
+        pass
+
+    def parse(self,input):
+        """Parse the given `input` (a string) using the given rules and lexer.
+
+        Note that some LALR parser generators produce parsers that will return
+        an Abstract Syntax Tree (AST) as a result, allowing the caller to
+        walk the tree and produce the desired final product. However, this
+        parser does not do that.
+
+        Instead, this function will return nothing, but will produce the
+        desired final product by means of the `action` functions given in
+        ``Parser.addrule``. 
+
+        It is also important to note that parsing will proceed using an LALR
+        table-based parser, which is not the most efficient form of parsing
+        (although it is very well suited to a wide array of problems). As a
+        result, parsing may be very slow for large inputs.
+        """
+        pass
