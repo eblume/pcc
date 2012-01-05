5a2d0c7f93ce782485289074e564ce98cd9f3495
Erich Blume <blume.erich@gmail.com>
2012-01-05 00:50:17 -0800

    Added doc to facade.__init__.py - starting move to pcc

--- facade/__init__.py
+++ facade/__init__.py
@@ -1,4 +1,11 @@
+"""pcc - Create parsers for context free grammars (CFGs).
 
+See ``pcc.lexer`` for a lexical analyzer/tokenizer class.
+
+See ``pcc.parser`` for several implementations of CFG parser generators that
+utilize the ``Lexer`` class from ``pcc.lexer``.
+
+"""
 
 VERSION_INFO=(0,0)
 
