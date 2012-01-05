a90746ecb6cdb12821cc0730713006a2ef115932
Erich Blume <blume.erich@gmail.com>
2012-01-04 20:31:31 -0800

    use ABC for parser.Parser, commented out doctest

--- facade/parser.py
+++ facade/parser.py
@@ -14,51 +14,73 @@ setup as a way to read through the 'Purple Dragon Book' - Aho, Lam, Sethi, and
 Ullman.
 """
 
+from abc import ABCMeta,abstractmethod
 
-class Parser:
-    def __init__(self, lexer):
-        """LALR parser of arbitrary BNF-like rules.
-    
-        Use ``addrule`` to add rules with semantic actions. Use ``parse`` to
-        execute parsing. Parsing is immediate and returns no value, unlike other
-        LALR parsers which might work stepwise - this is because this class takes
-        advantage of python generator functions.
-    
-        The following example emulates this simple grammar (in bison/YACC form:)
-    
-        expr : expr '+' term   { $$ = $1 + $3; }
-             | expr '-' term   { $$ = $1 - $3; }
-             | term            { $$ = $1; }
-             ;
+class GrammarError(Exception):
+    """Exception raised by a parser.Parser object *before* parsing begins."""
+    pass
+
+class SyntaxError(Exception):
+    "Exception raised by a parser.Parser object when a syntax error occurs."
+    pass
+
+def parser():
+    """Create a parser.Parser object using the default algorithm."""
         
-        term : '(' expr ')'    { $$ = $2; }
-             | num             { $$ = $1; }
-             ;
-    
-        num : '0'              {$$ = 0; }
-            | '1'              {$$ = 1; }
-            [.... etc, for each number 0-0 ....]
-            ;
-    
-        >>> l = Lexer()
-        >>> l.addtoken('NUMBER',r'[0-9]+')
-        >>> p = Parser(l)
-        >>>
-        >>> p.addrule('prog', "expr", lambda v: print(v[0]))
-        >>> 
-        >>> p.addrule('expr', "expr '+' term", lambda v: v[0] + v[2])
-        >>> p.addrule('expr', "expr '-' term", lambda v: v[0] - v[2])
-        >>> p.addrule('expr', "term", lambda v: v[0])
-        >>>
-        >>> p.addrule('term', " '(' expr ')' ", lambda v: v[1])
-        >>> p.addrule('term', " NUMBER ", lambda v: v[0])
-        >>>
-        >>> p.parse("5-(9+2)")
-        -6
-    
-        """
-        pass
 
+class Parser(metaclass=ABCMeta):
+    """CFG parser of arbitrary BNF-like rules.
+
+    Actual implementations should subclass this abstract base class, and may
+    have slightly different levels of 'computational power'. That is to say,
+    an LL language is slightly less expressive than an SLR, which is less
+    expressive than an LALR, which is less expressive than an LR language.
+    All are essentially 'context free grammars'. If a parser encounters an
+    error before parsing has begun (ie. an error with a rule) then
+    ``parser.GrammarError`` should be raised. If a parser encounters a syntax
+    error during parsing, then ``parser.SyntaxError`` should be raised. Other
+    errors should be reported in the most reasonably expressive manner possible.
+
+    Use ``addrule`` to add rules with semantic actions. Use ``parse`` to
+    execute parsing. Parsing is immediate and returns no value, unlike other
+    LALR parsers which might work stepwise - this is because this class takes
+    advantage of python generator functions.
+
+    The following example emulates this simple grammar (in bison/YACC form:)
+
+    expr : expr '+' term   { $$ = $1 + $3; }
+         | expr '-' term   { $$ = $1 - $3; }
+         | term            { $$ = $1; }
+         ;
+    
+    term : '(' expr ')'    { $$ = $2; }
+         | num             { $$ = $1; }
+         ;
+
+    num : '0'              {$$ = 0; }
+        | '1'              {$$ = 1; }
+        [.... etc, for each number 0-0 ....]
+        ;
+
+    >> l = Lexer()
+    >> l.addtoken('NUMBER',r'[0-9]+')
+    >> p = parser(l)
+    >>
+    >> p.addrule('prog', "expr", lambda v: print(v[0]))
+    >> 
+    >> p.addrule('expr', "expr '+' term", lambda v: v[0] + v[2])
+    >> p.addrule('expr', "expr '-' term", lambda v: v[0] - v[2])
+    >> p.addrule('expr', "term", lambda v: v[0])
+    >>
+    >> p.addrule('term', " '(' expr ')' ", lambda v: v[1])
+    >> p.addrule('term', " NUMBER ", lambda v: v[0])
+    >>
+    >> p.parse("5-(9+2)")
+    -6
+
+    """
+
+    @abstractmethod
     def addrule(self,symbol,rule,action):
         """Add a production (rule) to the grammar of the parser. Instructs the
         parser that `symbol` can be derived using `rule`, producing a result
@@ -123,12 +145,13 @@ class Parser:
         token specificically for that keyword instead.
 
         """
-        pass
+        raise NotImplementedError("Attempt to call an abstract method.")
 
+    @abstractmethod
     def parse(self,input):
         """Parse the given `input` (a string) using the given rules and lexer.
 
-        Note that some LALR parser generators produce parsers that will return
+        Note that some parser generators produce parsers that will return
         an Abstract Syntax Tree (AST) as a result, allowing the caller to
         walk the tree and produce the desired final product. However, this
         parser does not do that.
@@ -136,10 +159,6 @@ class Parser:
         Instead, this function will return nothing, but will produce the
         desired final product by means of the `action` functions given in
         ``Parser.addrule``. 
-
-        It is also important to note that parsing will proceed using an LALR
-        table-based parser, which is not the most efficient form of parsing
-        (although it is very well suited to a wide array of problems). As a
-        result, parsing may be very slow for large inputs.
         """
         pass
+
